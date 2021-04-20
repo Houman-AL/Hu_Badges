4f0ad6c97f7cd62bbd47d8b7ddbad9ce64720817
Houman AL <82032044+Houman-AL@users.noreply.github.com>
2021-04-20 23:56:42 +0430

    Update fxmanifest.lua

--- Hu_Badges/fxmanifest.lua
+++ Hu_Badges/fxmanifest.lua
@@ -1,32 +1,32 @@
 ----------------------------------
 ------------- Houman -------------
 ----------------------------------
---[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
-                --[[ Houman ]]--                 
---[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
-fx_version 'bodacious'--[[ Houman ]]--
-games { 'gta5' }--[[ Houman ]]--
---[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
-                --[[ Houman ]]--                 
---[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
-author 'Houman#7172'--[[ Houman ]]--
-version '1.0.0'--[[ Houman ]]--
---[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
-                --[[ Houman ]]--                 
---[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
-client_script {--[[ Houman ]]--
-    'client.lua'--[[ Houman ]]--
-}--[[ Houman ]]----[[ Houman ]]--
---[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
-                --[[ Houman ]]--                 
---[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
-server_script {--[[ Houman ]]--
-    'server.lua'--[[ Houman ]]--
-}--[[ Houman ]]----[[ Houman ]]--
---[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
-                --[[ Houman ]]--                 
---[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
+--[Made By Houman]]--
+                --[ Houman ]]--                 
+--[Made By Houman]]--
+fx_version 'bodacious'--[ Houman ]]--
+games { 'gta5' }--[ Houman ]]--
+--[Made By Houman]]--
+                --[ Houman ]]--                 
+--[Made By Houman]]--
+author 'Houman#7172'--[ Houman ]]--
+version '1.0.0'--[ Houman ]]--
+--[Made By Houman]]--
+                --[ Houman ]]--                 
+--[Made By Houman]]--
+client_script {--[ Houman ]]--
+    'client.lua'--[ Houman ]]--
+}--[ Houman ]]----[ Houman ]]--
+--[Made By Houman]]--
+                --[ Houman ]]--                 
+--[Made By Houman]]--
+server_script {--[ Houman ]]--
+    'server.lua'--[ Houman ]]--
+}--[ Houman ]]----[ Houman ]]--
+--[Made By Houman]]--
+                --[ Houman ]]--                 
+--[Made By Houman]]--
 shared_script 'config.lua'
---[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
-                --[[ Houman ]]--                 
---[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
+--[Made By Houman]]--
+                --[ Houman ]]--                 
+--[Made By Houman]]--
