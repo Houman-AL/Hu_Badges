8c26ddef7d7fa967b12dd4eb0afcbf6bad3524b9
Houman AL <82032044+Houman-AL@users.noreply.github.com>
2021-04-20 21:10:13 +0430

    Add files via upload

---
+++ Hu_Badges/config.lua
@@ -0,0 +1,44 @@
+
+--[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
+                --[ Houman ]]--                 
+--[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
+
+Config = {}
+
+Config.ToggleBadgeJobsCommand = 'badge'
+
+Config.ToggleBadgeJobsCommandinfo = 'toggle badge'
+
+Config.up_head = 0.6
+
+Config.AllPlayers = 5000 -- plz dont changit
+
+Config.BageJobs = {
+    -- {  job = your job need, color = { r= red, g = green, b = blue }  } (r, g, b < 255) (r, g, b > 0)
+    {job='police', color={r=10, g=10, b=200}},
+    {job='fbi', color={r=0, g=0, b=255}},
+    {job='ambulance', color={r=222, g=10, b=10}},
+    {job='army', color={r=0, g=255, b=10}},
+    {job='mechanic', color={r=200, g=200, b=200}},
+    {job='taxi', color={r=255, g=210, b=10}},
+}
+
+Config.grades_info = {
+
+    { job = 'police',
+    grades = {
+--        [1] = {name='Cadet', color={r=20, g=20, b=255}}, -- its grade 1 police
+--        [2] = {name='Rank 2', color={r=15, g=15, b=255}}, -- its grade 2 police
+  --      [3] = {name='Rank 3', color={r=10, g=15, b=255}}, -- its grade 3 police
+    --    [4] = {name='Rank 4', color={r=10, g=10, b=255}}, -- its grade 4 police
+      --  [5] = {name='Rank 5', color={r=5, g=10, b=255}}, -- its grade 5 police
+        --[6] = {name='Rank 6', color={r=0, g=10, b=255}}, -- its grade 6 police
+        --[7] = {name='Rank 7', color={r=0, g=5, b=255}}, -- its grade 7 police
+        [8] = {name=nil, color={r=0, g=0, b=255}}, -- its grade 8 police
+    }},-- if name=nil then its show defalt grade name in database
+
+}
+
+--[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
+                --[ Houman ]]--                 
+--[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
---
+++ Hu_Badges/fxmanifest.lua
@@ -0,0 +1,32 @@
+----------------------------------
+------------- Houman -------------
+----------------------------------
+--[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
+                --[[ Houman ]]--                 
+--[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
+fx_version 'bodacious'--[[ Houman ]]--
+games { 'gta5' }--[[ Houman ]]--
+--[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
+                --[[ Houman ]]--                 
+--[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
+author 'Houman#7172'--[[ Houman ]]--
+version '1.0.0'--[[ Houman ]]--
+--[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
+                --[[ Houman ]]--                 
+--[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
+client_script {--[[ Houman ]]--
+    'client.lua'--[[ Houman ]]--
+}--[[ Houman ]]----[[ Houman ]]--
+--[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
+                --[[ Houman ]]--                 
+--[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
+server_script {--[[ Houman ]]--
+    'server.lua'--[[ Houman ]]--
+}--[[ Houman ]]----[[ Houman ]]--
+--[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
+                --[[ Houman ]]--                 
+--[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
+shared_script 'config.lua'
+--[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
+                --[[ Houman ]]--                 
+--[[Hu_Leak : https://discord.gg/2AQCXpbv95 ]]--
